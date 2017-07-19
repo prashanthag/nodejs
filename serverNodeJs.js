02b8867c4381df2c786014dfbc4f41693c07cd86
prashanthag <prashantha.g@gmail.com>
2017-07-19 00:21:57 -0700

    Update serverNodeJs.js

--- serverNodeJs.js
+++ serverNodeJs.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 var finalhandler = require('finalhandler');
 var serveStatic = require('serve-static');
-
-var serve = serveStatic("./");
+var serve = serveStatic("./",{'index': ['samples/live-streaming/live-delay-comparison-custom-manifest.html', 'index.htm']});
+//var serve = serveStatic("./");
 
 var server = http.createServer(function(req, res) {
   var done = finalhandler(req, res);
