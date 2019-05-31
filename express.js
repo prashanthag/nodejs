c979f877368595c09632ff3685c456b4521009d5
prashanthag <prashantha.g@gmail.com>
2019-05-30 21:13:42 -0700

    Create express.js

---
+++ express.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 80
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(function(req, res, next) {
+       res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+       res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+          next();
+    });
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
